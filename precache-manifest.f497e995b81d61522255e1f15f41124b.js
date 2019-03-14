56f259863c02177545bdd6e571fa51eca185269e
Tim <tim.lisiecki@gmail.com>
2019-03-14 16:14:20 -0400

    Updates

---
+++ precache-manifest.f497e995b81d61522255e1f15f41124b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "05a7b0bc102d340e5574",
+    "url": "/robofriends-with-redux/static/css/main.449ae908.chunk.css"
+  },
+  {
+    "revision": "05a7b0bc102d340e5574",
+    "url": "/robofriends-with-redux/static/js/main.679be359.chunk.js"
+  },
+  {
+    "revision": "1c1d21ea9b38949b5db6",
+    "url": "/robofriends-with-redux/static/js/runtime~main.8ac0c489.js"
+  },
+  {
+    "revision": "c51f0622df1a09837416",
+    "url": "/robofriends-with-redux/static/css/2.64f524bc.chunk.css"
+  },
+  {
+    "revision": "c51f0622df1a09837416",
+    "url": "/robofriends-with-redux/static/js/2.bf19b1f6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-with-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c42335a064afb72dfced5033d6ccb6d5",
+    "url": "/robofriends-with-redux/index.html"
+  }
+];
